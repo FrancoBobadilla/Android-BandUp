c9d306fe59854093c03c1153d9e81fee041ef2a0
SantiagoDiazGonzalez <sandiazdiez@gmail.com>
2019-04-18 22:09:33 -0300

    login y sign up con funcionalidad basica

--- app/src/main/java/com/example/bandup/SignUpActivity.java
+++ app/src/main/java/com/example/bandup/SignUpActivity.java
@@ -1,63 +1,51 @@
 package com.example.bandup;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
 
-    private Button buttonLogin;
-    private TextView signUp;
     private EditText textEmail;
     private EditText textPassword;
+    private Button buttonSignUp;
     private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
 
+        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
-        //Referencias a las views
-        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        //Referenciar los views
         textEmail = (EditText) findViewById(R.id.textEmail);
         textPassword = (EditText) findViewById(R.id.textPassword);
-
-        //hipervinculo Texto de registrarse
-        signUp = (TextView)findViewById(R.id.buttonRegister);
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
-            }
-        });
+        buttonSignUp = (Button) findViewById(R.id.buttonSignUp);
 
         progressDialog = new ProgressDialog(this);
 
-        //listener del boton login
-        buttonLogin.setOnClickListener(this);
-
+        //listener del boton
+        buttonSignUp.setOnClickListener(this);
     }
 
-    private void signIn(){
+    private void createAcount(){
         //obtener email y password
         String email = textEmail.getText().toString().trim();
         String password = textPassword.getText().toString().trim();
@@ -72,23 +60,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return;
         }
 
-        progressDialog.setMessage("Ingresando");
+        progressDialog.setMessage("Signing Up");
         progressDialog.show();
 
-        //Login
-        mAuth.signInWithEmailAndPassword(email, password)
+        //registrar usuario en firebase
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(MainActivity.this, "Login Exitoso, Bienvenido", Toast.LENGTH_LONG).show();
+                            Toast.makeText(SignUpActivity.this, "Se ha registrado el usuario ", Toast.LENGTH_LONG).show();
                             FirebaseUser user = mAuth.getCurrentUser();
                             //updateUI(user);
                         } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(MainActivity.this, "Hubo un error en el login", Toast.LENGTH_LONG).show();
-                            //updateUI(null);
+                            //Si ya existe el usuario
+                            if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(SignUpActivity.this, "El usuario ya existe", Toast.LENGTH_LONG).show();
+                            }else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(SignUpActivity.this, "No se pudo registrar el usuario ", Toast.LENGTH_LONG).show();
+                                //updateUI(null);
+                            }
                         }
                         progressDialog.dismiss();
                     }
@@ -97,6 +90,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        signIn();
+        //se invoca el metodo
+        createAcount();
     }
 }
